d1f759e9f2730b22b9e2b79da136d66e5f869a8c
yagnaSai94 <>
2022-12-28 14:04:10 +0530

    Added SampleScreen

---
+++ lib/sample_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SampleScreen extends StatelessWidget {
+  const SampleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
