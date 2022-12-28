427c4d947905c35321a858bb3606445f51411db8
yagnaSai94 <>
2022-12-28 14:05:44 +0530

    Modified Sample Screen

--- lib/sample_screen.dart
+++ lib/sample_screen.dart
@@ -5,6 +5,10 @@ class SampleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [Text("data")],
+      ),
+    );
   }
 }
